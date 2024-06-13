ad0e99b35c153ace92b33a2c3fde7767e3f5fffd
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-06-13 23:18:51 +0530

    Creating stream

---
+++ StreamAPIDetail.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamAPIDetail {
+    public static void main(String[] args) {
+        // Creating Streams
+        List<String> myList = Arrays.asList("apple", "banana", "orange");
+        Stream<String> sequentialStream = myList.stream();
+        Stream<String> parallStream = myList.parallelStream();
+        // ----------- Creates a stream from an array
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        IntStream intStream = Arrays.stream(numbers);
+        // -----------------
+        Stream<String> strStream = Stream.of("apple", "banana", "orange");
+        // -----------------
+    }
+}

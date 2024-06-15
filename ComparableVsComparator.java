372834b1016b84426159e2469a3fbd5e453c0afd
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-06-15 12:49:49 +0530

    implement Comparable interface

---
+++ ComparableVsComparator.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Movie implements Comparable<Movie> {
+    private double rating;
+    private String name;
+    private int year;
+
+    public int compareTo(Movie m) {
+        return this.year - m.year;
+    }
+
+    public Movie(double rating, String name, int year) {
+        this.rating = rating;
+        this.name = name;
+        this.year = year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+public class ComparableVsComparator {
+    public static void main(String[] args) {
+        ArrayList<Movie> list = new ArrayList<Movie>();
+        list.add(new Movie(8.3, "Force Awakens", 2015));
+        list.add(new Movie(8.7, "Star Wars", 1977));
+        list.add(new Movie(8.8, "Empire Strikes Back", 1980));
+        list.add(new Movie(8.4, "Return of the Jedi", 1983));
+
+        System.out.println("Movies after sorting : ");
+        Collections.sort(list);
+        for (Movie movie : list) {
+            System.out.println(movie.getName() + " " +
+                    movie.getRating() + " " +
+                    movie.getYear());
+        }
+    }
+}

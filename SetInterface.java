dec56241737e1656244c2e007fac775ba6a5189e
Lalit Siraswa <50705460+lalitsiraswa@users.noreply.github.com>
2024-07-01 11:24:01 +0000

    LinkedHashSet

---
+++ SetInterface.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class SetInterface {
+    public static void main(String[] args) {
+        System.out.println("------ HashSet -------");
+        // Java program to demonstrate the
+        // working of a HashSet
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("Geeks");
+        hashSet.add("For");
+        hashSet.add("Geeks");
+        hashSet.add("Is");
+        hashSet.add("Very helpful");
+        hashSet.add(null);
+        Iterator<String> iterator = hashSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + "  ");
+        }
+
+        System.out.println("------- LinkedHashSet --------");
+        // Java program to demonstrate the
+        // working of a LinkedHashSet
+        LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+        lhs.add("Geeks");
+        lhs.add("For");
+        lhs.add("Geeks");
+        lhs.add("Is");
+        lhs.add("Very helpful");
+        iterator = lhs.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + "  ");
+        }
+    }
+}

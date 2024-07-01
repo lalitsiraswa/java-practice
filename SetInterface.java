65bb8f5e3890eac0dd4bd4348e6e9ac390a02afa
Lalit Siraswa <50705460+lalitsiraswa@users.noreply.github.com>
2024-07-01 11:30:55 +0000

    Sorted Set Interface 'TreeSet'

--- SetInterface.java
+++ SetInterface.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class SetInterface {
     public static void main(String[] args) {
@@ -29,6 +30,20 @@ public class SetInterface {
         lhs.add("Is");
         lhs.add("Very helpful");
         iterator = lhs.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + "  ");
+        }
+
+        System.out.println("------ Sorted Set Interface -------");
+        // Java program to demonstrate the
+        // working of a TreeSet
+        TreeSet<String> ts = new TreeSet<String>();
+        ts.add("Geeks");
+        ts.add("For");
+        ts.add("Geeks");
+        ts.add("Is");
+        ts.add("Very helpful");
+        iterator = ts.iterator();
         while (iterator.hasNext()) {
             System.out.print(iterator.next() + "  ");
         }

d21dd66d4ce33bd448ac91db8ce343cebc72e4e1
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-03 22:10:20 +0530

    ThreadImplementation

--- ThreadImplementation.java
+++ ThreadImplementation.java
@@ -27,11 +27,24 @@ class Thread2 implements Runnable {
 public class ThreadImplementation {
     public static void main(String[] args) {
         System.out.println("Main is starting...");
-        Thread thread1 = new Thread1("Thread-1");
+        // Thread thread1 = new Thread1("Thread-1");
         // thread1.setDaemon(true);
-        thread1.start();
+        // thread1.start();
 
-        Thread thread2 = new Thread(new Thread2(), "Thread-2");
+        // Thread thread2 = new Thread(new Thread2(), "Thread-2");
+        // Runnable runnable = () -> {
+        // for (int i = 1; i <= 5; i++) {
+        // System.out.println("Inside " + Thread.currentThread() + " : " + i);
+        // }
+        // };
+        // Thread thread2 = new Thread(runnable);
+        // thread2.start();
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Inside " + Thread.currentThread() + " : " + i);
+            }
+        }, "Thread-2");
         thread2.start();
         System.out.println("Main is exiting...");
     }

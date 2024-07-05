13d2e4c86477b4ed18ff58349f8e94c89aafb7d4
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-05 23:06:24 +0530

    HashMapImplRevise

--- HasnMapImplementationRevision.java
+++ HasnMapImplementationRevision.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class HashMapImpl<K, V> {
-    private List<MapNode<K, V>> bucket;
+class HashMapImplRevise<K, V> {
+    private List<HashMapNode<K, V>> bucket;
     private int capacity; // length of the bucket
     private int size; // number of elements in the map
     private final int INITIAL_CAPACITY = 5; // initial length of the bucket array
 
-    public HashMapImpl() {
-        bucket = new ArrayList<MapNode<K, V>>();
-        capacity = INITIAL_CAPACITY;
+    public HashMapImplRevise() {
+        bucket = new ArrayList<HashMapNode<K, V>>();
+        this.capacity = INITIAL_CAPACITY;
         // Below one is important step
         // if we are not adding element into the list, the list size will be zero(0)
         // only, that/s why we need to add null.
@@ -19,27 +19,13 @@ class HashMapImpl<K, V> {
     }
 
     private int getBucketIndex(K key) {
-        // Object class has this 'hashCode()' function, the hashCode() basically return
-        // the memory address of the Object
         int hashCode = key.hashCode();
         return hashCode % capacity;
     }
 
-    public V get(K key) {
-        int bucketIndex = getBucketIndex(key);
-        MapNode<K, V> head = bucket.get(bucketIndex);
-        while (head != null) {
-            if (head.key.equals(key)) {
-                return head.value;
-            }
-            head = head.next;
-        }
-        return null;
-    }
-
     public void put(K key, V value) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K, V> head = bucket.get(bucketIndex);
+        HashMapNode<K, V> head = bucket.get(bucketIndex);
         while (head != null) {
             if (head.key.equals(key)) {
                 head.value = value;
@@ -48,15 +34,12 @@ class HashMapImpl<K, V> {
             head = head.next;
         }
         size++;
-        MapNode<K, V> newEntry = new MapNode<K, V>(key, value);
+        HashMapNode<K, V> newEntry = new HashMapNode<K, V>(key, value);
         head = bucket.get(bucketIndex);
         newEntry.next = head;
         bucket.set(bucketIndex, newEntry);
 
-        // (n/b) load factor -> n : number of entries in the hashmap
-        // b : number of buckets
         double loadFactor = (1.0 * size) / capacity;
-
         System.out.println("inserting key : " + key);
         System.out.println("load factor : " + loadFactor);
         if (loadFactor > 0.7) {
@@ -66,15 +49,15 @@ class HashMapImpl<K, V> {
 
     private void rehash() {
         System.out.println("Rehashing buckets");
-        List<MapNode<K, V>> temp = bucket;
-        bucket = new ArrayList<MapNode<K, V>>();
+        List<HashMapNode<K, V>> temp = bucket;
         capacity *= 2;
-        for (int i = 0; i < capacity; i++)
+        bucket = new ArrayList<HashMapNode<K, V>>();
+        for (int i = 0; i < capacity; i++) {
             bucket.add(null);
+        }
         size = 0;
-
         for (int i = 0; i < temp.size(); i++) {
-            MapNode<K, V> head = temp.get(i);
+            HashMapNode<K, V> head = temp.get(i);
             while (head != null) {
                 put(head.key, head.value);
                 head = head.next;
@@ -82,41 +65,55 @@ class HashMapImpl<K, V> {
         }
     }
 
-    public void remove(K key) {
+    public V get(K key) {
         int bucketIndex = getBucketIndex(key);
-        MapNode<K, V> head = bucket.get(bucketIndex);
-        MapNode<K, V> previousNode = null;
+        HashMapNode<K, V> head = bucket.get(bucketIndex);
         while (head != null) {
             if (head.key.equals(key)) {
-                if (previousNode == null)
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        int bucketIndex = getBucketIndex(key);
+        HashMapNode<K, V> head = bucket.get(bucketIndex);
+        HashMapNode<K, V> previousNode = null;
+        while (head != null) {
+            if (head.key.equals(key)) {
+                if (previousNode == null) {
                     bucket.set(bucketIndex, head.next);
-                else
+                    return;
+                } else {
                     previousNode.next = head.next;
-                head.next = null;
-                size--;
-                break;
+                    head = null;
+                    size--;
+                    break;
+                }
             }
             previousNode = head;
             head = head.next;
         }
     }
 
-    private class MapNode<K, V> {
+    private class HashMapNode<K, V> {
         K key;
         V value;
-        MapNode<K, V> next;
+        HashMapNode<K, V> next;
 
-        public MapNode(K key, V value) {
+        public HashMapNode(K key, V value) {
             this.key = key;
             this.value = value;
-            next = null;
+            this.next = null;
         }
     }
 }
 
-public class HashMapImplementation {
+public class HasnMapImplementationRevision {
     public static void main(String[] args) {
-        HashMapImpl<Integer, String> hashMap = new HashMapImpl<Integer, String>();
+        HashMapImplRevise<Integer, String> hashMap = new HashMapImplRevise<Integer, String>();
         hashMap.put(1, "Lalit");
         hashMap.put(6, "Krishna");
         hashMap.put(5, "Jyoti");
@@ -128,5 +125,7 @@ public class HashMapImplementation {
         System.out.println(hashMap.get(6));
         System.out.println(hashMap.get(3));
         System.out.println(hashMap.get(7));
+
+        hashMap.remove(1);
     }
 }

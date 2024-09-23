2428ab39af0f2100dc3637be1b27e9efa25002d6
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-09-23 23:03:00 +0530

    stream example

--- Streams.java
+++ Streams.java
@@ -10,5 +10,14 @@ public class Streams {
         List<Integer> evenNum = numList.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
         System.out.println("Printing the list after stream operation : " + numList);
         System.out.println("Printing the even list after stream operation : " + evenNum);
+        // --------------------------------------
+        List<Integer> marks = new ArrayList<>(List.of(30, 78, 26, 96, 79));
+        System.out.println("Marks fo students before grace : " + marks);
+        // Now we want to grace marks by 6 using the streams to process over processing
+        // collection
+        // Using stream, we map every object and later collect to List and store them
+        List<Integer> updatedMarks = marks.stream().map(x -> x + 9).collect(Collectors.toList());
+        System.out.println();
+        System.out.println("Marks of students after grace : " + updatedMarks);
     }
 }

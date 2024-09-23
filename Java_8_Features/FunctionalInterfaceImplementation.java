fb51b449008e293f225396a58b3874af426543ac
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-09-23 19:28:07 +0530

    Before Java 8, we had to create anonymous inner class objects or implement these interfaces.

--- Java_8_Features/FunctionalInterfaceImplementation.java
+++ Java_8_Features/FunctionalInterfaceImplementation.java
@@ -1,3 +1,12 @@
 public class FunctionalInterfaceImplementation {
-    
+    public static void main(String[] args) {
+        // Before Java 8, we had to create anonymous inner class objects or implement
+        // these interfaces.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("New thread created");
+            }
+        }).start();
+    }
 }

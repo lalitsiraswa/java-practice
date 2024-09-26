dbeccc69df6030e9383b852a969d9c6d30db581a
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-09-26 15:47:54 +0530

    Class & Object concept understand

--- ObjectOrientedProgrammingConcept/ClassExample.java
+++ ObjectOrientedProgrammingConcept/ClassExample.java
@@ -15,9 +15,52 @@ class GFG {
     }
 }
 
+// Class Declaration
+class Dog {
+    // Instance Variables
+    String name;
+    String breed;
+    int age;
+    String color;
+
+    // Constructor Declaration of class
+    public Dog(String name, String breed, int age, String color) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return ("Hi my name is " + this.getName()
+                + ".\nMy breed,age and color are "
+                + this.getBreed() + "," + this.getAge()
+                + "," + this.getColor());
+    }
+}
+
 public class ClassExample {
     public static void main(String[] args) {
         GFG.set("Rathore Avinash", 2000000);
         GFG.get();
+        Dog tuffy = new Dog("Tuffy", "Paplillon", 5, "White");
+        System.out.println(tuffy);
     }
 }

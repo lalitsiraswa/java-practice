b7d6d9c4926e7b48aa94955f8b18df2575c71388
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-10-02 12:01:06 +0530

    OverloadingAndOverriding

---
+++ object_oriented_programming_concept/AbstractionImplementation.java
@@ -0,0 +1,171 @@
+package object_oriented_programming_concept;
+
+import org.w3c.dom.css.Rect;
+
+// Java program to illustrate the
+// concept of Abstraction
+abstract class Shape {
+    String color;
+
+    // these are abstract methods
+    abstract double area();
+
+    public abstract String toString();
+
+    // abstract class can have the constructor
+    protected Shape(String color) {
+        System.out.println("Shape constructor called");
+        this.color = color;
+    }
+
+    // this is a concrete method
+    public String getColor() {
+        return color;
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    public Circle(String color, double radius) {
+        // calling Shape constructor
+        super(color);
+        System.out.println("Circle constructor called");
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle color is : " + super.getColor() + " and area is : " + area();
+    }
+}
+
+class Rectangle extends Shape {
+
+    double length;
+    double width;
+
+    public Rectangle(String color, double length, double width) {
+        // calling Shape constructor
+        super(color);
+        System.out.println("Rectangle constructor called");
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle color is " + super.getColor()
+                + "and area is : " + area();
+    }
+}
+
+// Java Program to implement
+// Java Abstraction
+
+// Abstract Class declared
+abstract class Animal {
+    private String name;
+
+    protected Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void makeSound();
+
+    public String getName() {
+        return name;
+    }
+}
+
+// Abstracted class
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void makeSound() {
+        System.out.println(getName() + " barks");
+    }
+}
+
+// Abstracted class
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    public void makeSound() {
+        System.out.println(getName() + " meows");
+    }
+}
+
+// --------------------- Interface ----------------------
+// Define an interface named Shape
+interface ShapeInterface {
+    // Abstract method for calculating the area
+    double calculateArea();
+}
+
+// Implement the interface in a class named Circle
+class CircleClass implements ShapeInterface {
+    private double radius;
+
+    // Constructor for circle
+    public CircleClass(double radius) {
+        this.radius = radius;
+    }
+
+    // Implementing the abstract method from the Shape
+    // interface
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
+
+// Implement the interface in a class named Rectangle
+class RectangleClass implements ShapeInterface {
+    private double length;
+    private double width;
+
+    // Constructor for Rectangle
+    public RectangleClass(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // Implementing the abstract method from the Shape
+    // interface
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+public class AbstractionImplementation {
+    public static void main(String[] args) {
+        Shape s1 = new Circle("Red", 2.2);
+        System.out.println(s1);
+        Shape s2 = new Rectangle("Yellow", 2, 4);
+        System.out.println(s2);
+        // ------------------------
+        Animal myDog = new Dog("Buddy");
+        myDog.makeSound();
+        Animal myCat = new Cat("Fluffy");
+        myCat.makeSound();
+        // -------------------------
+        CircleClass circleClass = new CircleClass(5.0);
+        System.out.println("Area of Circle: " + circleClass.calculateArea());
+        RectangleClass rectangleClass = new RectangleClass(4.0, 6.0);
+        System.out.println("Area of Rectangle: " + rectangleClass.calculateArea());
+    }
+}
---
+++ object_oriented_programming_concept/OverloadingAndOverriding.java
@@ -0,0 +1,90 @@
+package object_oriented_programming_concept;
+
+// Method Overloading in Java
+// In Java, Method Overloading allows different methods to have the same name, but different signatures where the signature can differ by the number of input parameters or type of input parameters, or a mixture of both.
+// In Method overloading compared to the parent argument, the child argument will get the highest priority.
+// Java program to demonstrate working of method overloading in Java 
+
+class Sum {
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int sum(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    // Overloaded sum(). This sum takes two double
+    // parameters
+    public double sum(double x, double y) {
+
+        return x + y;
+    }
+}
+
+// Different Ways of Method Overloading in Java
+// 1. Changing the Number of Parameters.
+// 2. Changing Data Types of the Arguments.
+// 3. Changing the Order of the Parameters of Methods
+
+// Method overriding is one of the ways by which Java achieves Run Time
+// Polymorphism. The version of a method that is executed will be determined by
+// the object that is used to invoke it. If an object of a parent class is used
+// to invoke the method, then the version in the parent class will be executed,
+// but if an object of the subclass is used to invoke the method, then the
+// version in the child class will be executed. In other words, it is the type
+// of the object being referred to (not the type of the reference variable) that
+// determines which version of an overridden method will be executed.
+
+// Java program to demonstrate
+// method overriding in java
+class Parent {
+    void show() {
+        System.out.println("Parent's show()");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    void show() {
+        System.out.println("Child's show()");
+    }
+}
+
+// 1. Overriding and Access Modifiers
+// The access modifier for an overriding method can allow more, but not less,
+// access than the overridden method.
+class ParentClass {
+    private void m1() {
+        System.out.println("From parent m1()");
+    }
+
+    protected void m2() {
+        System.out.println("From parent m2()");
+    }
+}
+
+class ChildClass extends ParentClass {
+    private void m1() {
+        System.out.println("From child m1()");
+    }
+
+    @Override
+    public void m2() {
+        System.out.println("From child m2()");
+    }
+}
+
+public class OverloadingAndOverriding {
+    public static void main(String[] args) {
+        Sum s = new Sum();
+        System.out.println(s.sum(10, 20));
+        System.out.println(s.sum(10, 20, 30));
+        System.out.println(s.sum(10.5, 20.5));
+        // ---------------------------------------
+        Parent parent = new Parent();
+        parent.show();
+        Parent parent2 = new Child();
+        parent2.show();
+    }
+}

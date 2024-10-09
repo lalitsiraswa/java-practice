7f293af314b981fa1a61ca86d7692fcb8f6274d8
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-10-09 23:48:50 +0530

    throws in java

--- ThrowAndThrows.java
+++ ThrowAndThrows.java
@@ -1,3 +1,5 @@
+// Java throws
+// throws is a keyword in Java that is used in the signature of a method to indicate that this method might throw one of the listed type exceptions. The caller to these methods has to handle the exception using a try-catch block. 
 public class ThrowAndThrows {
     // Java program that demonstrates the use of throw
     static void fun() {
@@ -13,7 +15,13 @@ public class ThrowAndThrows {
         }
     }
 
-    public static void main(String[] args) {
+    // Java program to demonstrate working of throws
+    static void funThrows() throws IllegalAccessException {
+        System.out.println("Inside funThrows()");
+        throw new IllegalAccessException("Throws Demo");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         try {
             fun();
         } catch (NullPointerException ex) {
@@ -21,6 +29,14 @@ public class ThrowAndThrows {
         } finally {
             System.out.println("finally from main()");
         }
+        // --------------------------------------------
+        Thread.sleep(5000);
         System.out.println("End of the program");
+        // ---------------
+        try {
+            funThrows();
+        } catch (IllegalAccessException ex) {
+            System.out.println("Caught in main() - Throws");
+        }
     }
 }

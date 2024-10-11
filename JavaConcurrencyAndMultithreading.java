db0a93daff873a57430684be2f6c2eba5ba9e2a3
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-10-11 12:01:48 +0530

    Creating Thread By Extending Thread Class

--- JavaConcurrencyAndMultithreading.java
+++ JavaConcurrencyAndMultithreading.java
@@ -1,15 +1,24 @@
 class Thread1 extends Thread {
+    public Thread1(String threadName) {
+        super(threadName);
+    }
+
     @Override
     public void run() {
+        System.out.println(Thread.currentThread().getName() + " is starting");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Inside Thread1 : " + i);
+            System.out.println("Inside " + Thread.currentThread() + " : " + i);
         }
+        System.out.println(Thread.currentThread().getName() + " is exiting");
     }
 }
 
 public class JavaConcurrencyAndMultithreading {
     public static void main(String[] args) {
-        Thread thread1 = new Thread1();
+        System.out.println(Thread.currentThread().getName() + " is starting");
+        Thread thread1 = new Thread1("Thread-1");
+        // thread1.setDaemon(true);
         thread1.start();
+        System.out.println(Thread.currentThread().getName() + " is exiting");
     }
 }

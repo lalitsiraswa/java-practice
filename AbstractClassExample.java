5eb5fdb9224f3f17af3c02c690449c0e5222201b
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-10-16 22:01:28 +0530

    latest code push

---
+++ AbstractClassExample.java
@@ -0,0 +1,6 @@
+// Abstract class may have both abstract and non-abstract methods(methods with bodies).
+public class AbstractClassExample {
+    public static void main(String[] args) {
+
+    }
+}

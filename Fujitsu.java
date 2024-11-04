3b8b74b8bf27f58091dfa7aa4adda19e89eb93c3
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-11-04 22:09:40 +0530

    Update the code

--- Fujitsu.java
+++ Fujitsu.java
@@ -1,6 +1,6 @@
 class PrintSeries implements Runnable {
-    // Integer i = 1;
-    static Integer i = 1;
+    Integer i = 1;
+    // static Integer i = 1;
 
     @Override
     public void run() {
@@ -14,14 +14,14 @@ class PrintSeries implements Runnable {
 
 public class Fujitsu {
     public static void main(String[] args) {
-        // PrintSeries printSeries = new PrintSeries();
-        // Thread t1 = new Thread(printSeries);
-        // Thread t2 = new Thread(printSeries);
+        PrintSeries printSeries = new PrintSeries();
+        Thread t1 = new Thread(printSeries);
+        Thread t2 = new Thread(printSeries);
         // ------------------------------------------
-        PrintSeries printSeries1 = new PrintSeries();
-        PrintSeries printSeries2 = new PrintSeries();
-        Thread t1 = new Thread(printSeries1);
-        Thread t2 = new Thread(printSeries2);
+        // PrintSeries printSeries1 = new PrintSeries();
+        // PrintSeries printSeries2 = new PrintSeries();
+        // Thread t1 = new Thread(printSeries1);
+        // Thread t2 = new Thread(printSeries2);
         // ------------------------------------------
         t1.start();
         t2.start();

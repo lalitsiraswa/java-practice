4de700a5cd32b32d3ed656974c6f056b710f7197
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-11-29 18:33:29 +0530

    Fujitsu

--- Fujitsu.java
+++ Fujitsu.java
@@ -1,29 +1,41 @@
-class PrintSeries implements Runnable {
-    Integer i = 1;
-    // static Integer i = 1;
+class NumberPrinter {
+    private int currentNumber = 1;
+    private final int MAX_NUMBER = 10;
 
-    @Override
-    public void run() {
-        synchronized (i) {
-            while (i <= 10) {
-                System.out.println(i++);
+    public synchronized void printNumbers() {
+        while (currentNumber <= MAX_NUMBER) {
+            System.out.println(Thread.currentThread().getName() + " : " + currentNumber);
+            currentNumber++;
+            // Notify the other thread that it can take its turn
+            notify();
+            try {
+                // Wait if we haven't reached the maximum number, allowing the other thread to
+                // proceed
+                if (currentNumber <= MAX_NUMBER) {
+                    wait();
+                }
+            } catch (InterruptedException e) {
+                // Reset interrupted status
+                Thread.currentThread().interrupt();
             }
         }
     }
 }
 
+// In the code example provided, we’re using a method reference
+// (printer::printNumbers) to specify the task for each thread. The Thread
+// constructor accepts either a Runnable object or a lambda expression (or
+// method reference) that matches the Runnable functional interface, which has a
+// single run() method.
 public class Fujitsu {
     public static void main(String[] args) {
-        PrintSeries printSeries = new PrintSeries();
-        Thread t1 = new Thread(printSeries);
-        Thread t2 = new Thread(printSeries);
-        // ------------------------------------------
-        // PrintSeries printSeries1 = new PrintSeries();
-        // PrintSeries printSeries2 = new PrintSeries();
-        // Thread t1 = new Thread(printSeries1);
-        // Thread t2 = new Thread(printSeries2);
-        // ------------------------------------------
+        NumberPrinter printer = new NumberPrinter();
+        // Create two threads that will share the same NumberPrinter instance
+        Thread t1 = new Thread(printer::printNumbers, "Thread-1");
+        Thread t2 = new Thread(printer::printNumbers, "Thread-2");
+        // Start both threads
         t1.start();
         t2.start();
+
     }
 }
